151732f06262ea11b5322556d79632faa0ac1a6d
unknown <rsyourat@gmail.com>
2021-01-30 08:03:50 +0900

    image move

--- js/parallax.js
+++ js/parallax.js
@@ -1,37 +1,38 @@
 
 $(function(){
-    
-    // $('.box1').css('background-position','center ','top');
+
             $(window).scroll(function(){
                 var roll = $(this).scrollTop();
                 console.log(roll);
                 var window_h = window.innerHeight;
                 console.log(window_h);
 
-                // $('#bg2').css('position',);
-                if(roll/window_h > 0.4 && roll/window_h < 0.8){
+                if(roll/window_h > 0.2 && roll/window_h < 0.4){
+                    $('.box1').css('background-position','center top');
                     $('.box3').css('background-position','right '+(roll-window_h*0.4)+'px');
-                }else if(roll/window_h > 0.8){
-                    $('.box4').css('background-position','center '+(roll-window_h*0.6)+'px');
-                    // $('.img1').css('position','right '+(roll-window_h*0.4)+'px');
+                    $('.img1').css('top',(roll-window_h*0.9)+'px');
+                    // $('.img1').css('opacity',1)                
+                }else if(roll/window_h > 0.4 && roll/window_h < 0.7){
+                    $('.box3').css('background-position','right '+(roll-window_h*0.4)+'px');
+                    $('.img2').css('top',roll-window_h*1.1);
+                    $('.img1').css('top',0);
+                }else if(roll/window_h > 0.7){
+                    $('.box4').css('background-position','center '+(roll-window_h*0.4)+'px');
+                    $('.img2').css('top',roll-window_h*1.1);
                 }else{
                     $('.box3').css('background-position','right 0');
                     $('.box4').css('background-position','center 0');
+                    $('.img2').css('top',500);
                 }                
                 if(roll = window_h){
                     $('.box4').css('background-position','center bottom');
-                    $('.img1').css('position','right bottom');
                 }
-/*                 if(roll > 1600){
-                    $('#bg3').css('background-position','0 '+(roll-1600)+'px');
-                }else{
-                    $('#bg3').css('background-position','0 0');
-                } */
-/*                 if(roll > 1600){
-                    $('#contact').css('opacity', (roll-1600)/800);
-                    $('#contact').css('top',500);
+
+/*                 if(roll > 100){
+                    $('.img1').css('opacity', (roll-100)/50);
+                    $('.img1').css('top',50);
                   }else{
-                    $('#contact').css('opacity', 0);
+                    $('.img1').css('opacity', 0);
                   } */
             });
 });
