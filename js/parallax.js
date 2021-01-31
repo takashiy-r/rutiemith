e465fac5c8fa1f510ef9bd7fb8c373a92b2219db
unknown <rsyourat@gmail.com>
2021-01-31 18:52:14 +0900

    responcive

--- js/parallax.js
+++ js/parallax.js
@@ -2,7 +2,7 @@
 $(function(){
     var window_h = window.innerHeight;
     console.log(window_h);
-    $('.img1').css('top',window_h +'vh');
+    
     var kfOpc = 0;
     $('.img1').css('opacity',kfOpc);
 
@@ -13,7 +13,7 @@ $(function(){
                 // var rollstop = '90vh';
                 if(rollper < 0.2){
                     $('.box1').css('background-position','center top');
-                    $('.img1').css('top',rollper*100*0.5+'vh');
+                    $('.img1').css('top',rollper*100*0.7+'vh');
 
                     if(kfOpc < 0.8){
                         $('.img1').css('opacity',kfOpc+=0.1);
@@ -22,32 +22,25 @@ $(function(){
                 }else if(rollper >= 0.2 && rollper < 0.3){
                     $('.box1').css('background-position','center top');
                     $('.box3').css('background-position','right '+(rollper*100*0.1)+'vh');
-                    $('.img1').css('top',rollper*100*0.4+'vh');
+                    $('.img1').css('top',rollper*100*0.7+'vh');
 
                 }else if(rollper >= 0.3 && rollper < 0.7){
                     $('.box3').css('background-position','right '+(rollper*100*0.1)+'vh');
                     $('.img2').css('top',rollper*100*0.45+'vh');
-                    $('.img1').css('top',0);
 
                 }else if(rollper >= 0.7 && rollper < 1){
-                    $('.box4').css('background-position','center '+(rollper*100*0.1)+'vh');
+                    // $('.box4').css('background-position','center '+(rollper*100*0.1)+'vh');
                     $('.img2').css('top',rollper*100*0.45+'vh');
                     
                 }else if(rollper >= 1){
+                    // $('.img1').css('top',0);
                     $('.box3').css('background-position','right 0');
-                    $('.box4').css('background-position','center bottom');
+                    // $('.box4').css('background-position','center bottom');
                     $('.img2').css('position','right bottom');
                 }else{
 
                 }                
                 
-
-/*                 if(roll > 100){
-                    $('.img1').css('opacity', (roll-100)/50);
-                    $('.img1').css('top',50);
-                  }else{
-                    $('.img1').css('opacity', 0);
-                  } */
             });
             rollstop = rollper*100*0.1;
 });
