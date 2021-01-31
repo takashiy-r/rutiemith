791732dce449140cac1b260c005f72c9a236cad2
unknown <rsyourat@gmail.com>
2021-02-01 00:57:51 +0900

    add textimage

--- js/parallax.js
+++ js/parallax.js
@@ -12,7 +12,7 @@ $(function(){
                 var rollper = roll/window_h;
                 // var rollstop = '90vh';
                 if(rollper < 0.2){
-                    $('.box1').css('background-position','center top');
+                    $('.box1').css('background-position','center 30%');
                     $('.img1').css('top',rollper*100*0.7+'vh');
 
                     if(kfOpc < 0.8){
@@ -20,23 +20,23 @@ $(function(){
                     }
                             
                 }else if(rollper >= 0.2 && rollper < 0.3){
-                    $('.box1').css('background-position','center top');
+                    $('.box1').css('background-position','center 30%');
                     $('.box3').css('background-position','right '+(rollper*100*0.1)+'vh');
                     $('.img1').css('top',rollper*100*0.7+'vh');
 
                 }else if(rollper >= 0.3 && rollper < 0.7){
                     $('.box3').css('background-position','right '+(rollper*100*0.1)+'vh');
-                    $('.img2').css('top',rollper*100*0.45+'vh');
+                    // $('.img2').css('top',rollper*100*0.45+'vh');
 
                 }else if(rollper >= 0.7 && rollper < 1){
                     // $('.box4').css('background-position','center '+(rollper*100*0.1)+'vh');
-                    $('.img2').css('top',rollper*100*0.45+'vh');
+                    // $('.img2').css('top',rollper*100*0.45+'vh');
                     
                 }else if(rollper >= 1){
                     // $('.img1').css('top',0);
                     $('.box3').css('background-position','right 0');
                     // $('.box4').css('background-position','center bottom');
-                    $('.img2').css('position','right bottom');
+                    // $('.img2').css('position','right bottom');
                 }else{
 
                 }                
